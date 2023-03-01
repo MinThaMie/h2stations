9f0c979c7ab5cdb35a0eda921e2805e40eed8857
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2023-03-01 15:58:30 +0100

    Fix price because it's not part of the general API

--- netlify/functions/stations.js
+++ netlify/functions/stations.js
@@ -4,13 +4,13 @@ const ALL_STATIONS_API_ENDPOINT =
   'https://my.h2.live/ceemes/base.php?__type__=fuelstation&action=xmllist&__show_permclosed__=1&__language__=en&__transform__=json&__fieldsin__=idx,name,street,streetnr,zip,city,has_350_large,has_350_small,has_700_small,image,opening_hours,latitude,longitude,operatorname,operatorlogo,hostname,operatorhotline,comments,news,date_commissioning_message,progress_percent,progress_description,progress_extratext,countryshortname,funding,fundingpage,paymenttypes,paymentinfo,price_message,activity_message,tec_recom_time,openinghours_nextchange_message&__t__=b13831e6243b9bcd4c74aeda55c1fe22';
 
 const STATUS_700_SMALL_API_ENDPOINT =
-  'https://my.h2.live/ceemes/base.php?__type__=fuelstation&action=xmllist&__status_fueltype__=P700_SMALL&__show_permclosed__=1&__language__=en&__transform__=json&__fieldsin__=idx,name,combinedstatus,combinedremark&__t__=b13831e6243b9bcd4c74aeda55c1fe22';
+  'https://my.h2.live/ceemes/base.php?__type__=fuelstation&action=xmllist&__status_fueltype__=P700_SMALL&__show_permclosed__=1&__language__=en&__transform__=json&__fieldsin__=idx,name,combinedstatus,combinedremark,price_message&__t__=b13831e6243b9bcd4c74aeda55c1fe22';
 
 const STATUS_350_SMALL_API_ENDPOINT =
-  'https://my.h2.live/ceemes/base.php?__type__=fuelstation&action=xmllist&__status_fueltype__=P350_SMALL&__show_permclosed__=1&__language__=en&__transform__=json&__fieldsin__=idx,name,combinedstatus,combinedremark&__t__=b13831e6243b9bcd4c74aeda55c1fe22';
+  'https://my.h2.live/ceemes/base.php?__type__=fuelstation&action=xmllist&__status_fueltype__=P350_SMALL&__show_permclosed__=1&__language__=en&__transform__=json&__fieldsin__=idx,name,combinedstatus,combinedremark,price_message&__t__=b13831e6243b9bcd4c74aeda55c1fe22';
 
 const STATUS_350_LARGE_API_ENDPOINT =
-  'https://my.h2.live/ceemes/base.php?__type__=fuelstation&action=xmllist&__status_fueltype__=P350_LARGE&__show_permclosed__=1&__language__=en&__transform__=json&__fieldsin__=idx,name,combinedstatus,combinedremark&__t__=b13831e6243b9bcd4c74aeda55c1fe22';
+  'https://my.h2.live/ceemes/base.php?__type__=fuelstation&action=xmllist&__status_fueltype__=P350_LARGE&__show_permclosed__=1&__language__=en&__transform__=json&__fieldsin__=idx,name,combinedstatus,combinedremark,price_message&__t__=b13831e6243b9bcd4c74aeda55c1fe22';
 
 exports.handler = async (event, context) => {
   try {
@@ -36,6 +36,7 @@ exports.handler = async (event, context) => {
       if (station.countryshortname === 'NL') {
         let completeStation = { ...station };
         let shouldbeShown = false;
+        console.log(station.name);
         if (station.has_350_large == 't') {
           const s350l = status350Large.fuelstation.find(
             (s) => s.name == station.name
@@ -43,6 +44,8 @@ exports.handler = async (event, context) => {
           if (s350l && s350l.combinedstatus !== 'PLANNED') {
             completeStation['status350l'] = s350l.combinedstatus;
             completeStation['status350lmessage'] = s350l.combinedremark;
+            completeStation['price_message'] = s350l.price_message;
+            console.log(s350l.price_message)
             shouldbeShown = true;
           }
         }
@@ -53,6 +56,8 @@ exports.handler = async (event, context) => {
           if (s700 && s700.combinedstatus !== 'PLANNED') {
             completeStation['status700'] = s700.combinedstatus;
             completeStation['status700message'] = s700.combinedremark;
+            completeStation['price_message'] = s700.price_message;
+            console.log(s700.price_message);
             shouldbeShown = true;
           }
         }
@@ -63,6 +68,8 @@ exports.handler = async (event, context) => {
           if (s350s && s350s.combinedstatus !== 'PLANNED') {
             completeStation['status350s'] = s350s.combinedstatus;
             completeStation['status350smessage'] = s350s.combinedremark;
+            completeStation['price_message'] = s350s.price_message;
+            console.log(s350s.price_message);
             shouldbeShown = true;
           }
         }
