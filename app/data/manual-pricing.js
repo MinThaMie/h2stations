99b425d2e3253350c61a871b4d13b6059ad7382b
hans <hansvanherwijnen@resato.com>
2023-07-23 21:05:10 +0200

    Update Nieuwegein price

--- app/data/manual-pricing.js
+++ app/data/manual-pricing.js
@@ -1,7 +1,7 @@
 export const manualPricing = [
   {
     name: 'Hysolar Greenpoint - Nieuwegein',
-    price: '26,83*',
+    price: '26,12*',
     valuta: 'EUR',
     news: '* This information has been entered manually and may differ',
   },
